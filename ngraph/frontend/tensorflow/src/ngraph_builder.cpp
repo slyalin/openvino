98c50c1317bb6e0d2f7a2e6c51f1d5b3936b7e8e
jialipen <cecilia.peng@intel.com>
2021-04-01 00:54:02 +0800

    fix compiling error

--- ngraph/frontend/tensorflow/src/ngraph_builder.cpp
+++ ngraph/frontend/tensorflow/src/ngraph_builder.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
+#include <numeric>
 
 #include "graph.pb.h"
 #include "tensor.pb.h"
