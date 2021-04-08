0f61c22d424e5b375ea5a1c2054cf4496d1ec407
Michael Nosov <Mikhail.Nosov@intel.com>
2021-04-08 21:11:31 +0300

    More unit tests
    - Refactored basic_api tests to avoid code duplication for inputs/outputs API
    - Model-specific added real test for getPlaceByTensorName and overrideAll<Inputs/Outputs>

--- ngraph/test/frontend/paddlepaddle/basic_api.cpp
+++ ngraph/test/frontend/paddlepaddle/basic_api.cpp
@@ -15,6 +15,8 @@ using namespace ngraph::frontend;
 static const auto PDPD = "pdpd";
 static const std::string PATH_TO_MODELS = "/paddlepaddle/models/";
 
+// TODO: in future, move it to shared header file so that every frontend can run same test suite
+// ----- This is common part for all frontends -----
 using BasicTestParam = std::string;
 
 std::string getTestCaseName(const testing::TestParamInfo<BasicTestParam> &obj) {
@@ -25,12 +27,12 @@ std::string getTestCaseName(const testing::TestParamInfo<BasicTestParam> &obj) {
     return fileName;
 }
 
-class PDPDBasicTest : public ::testing::TestWithParam<BasicTestParam> {
+class FrontEndBasicTest : public ::testing::TestWithParam<BasicTestParam> {
 public:
     std::string                      m_modelFile;
     FrontEndManager                  m_fem;
-    std::shared_ptr<FrontEndPDPD>    m_frontEnd;
-    std::shared_ptr<InputModelPDPD>  m_inputModel;
+    FrontEnd::Ptr                    m_frontEnd;
+    InputModel::Ptr                  m_inputModel;
 
     void initParamTest() {
         m_modelFile = std::string(TEST_FILES) + PATH_TO_MODELS + GetParam();
@@ -46,146 +48,157 @@ public:
 
     void doLoadFromFile() {
         std::vector<std::string> frontends;
-        FrontEnd::Ptr fe;
         ASSERT_NO_THROW(frontends = m_fem.availableFrontEnds());
-        ASSERT_NO_THROW(fe = m_fem.loadByFramework(PDPD));
-        ASSERT_NO_THROW(m_frontEnd = std::dynamic_pointer_cast<FrontEndPDPD>(fe));
+        ASSERT_NO_THROW(m_frontEnd = m_fem.loadByFramework(PDPD));
         ASSERT_NE(m_frontEnd, nullptr);
-        ASSERT_NO_THROW(m_inputModel = std::dynamic_pointer_cast<InputModelPDPD>(m_frontEnd->loadFromFile(m_modelFile)));
+        ASSERT_NO_THROW(m_inputModel = m_frontEnd->loadFromFile(m_modelFile));
         ASSERT_NE(m_inputModel, nullptr);
     }
 };
 
-TEST_P(PDPDBasicTest, testLoadFromFile)
-{
+TEST_P(FrontEndBasicTest, testLoadFromFile) {
     ASSERT_NO_THROW(doLoadFromFile());
     std::shared_ptr<ngraph::Function> function;
     ASSERT_NO_THROW(function = m_frontEnd->convert(m_inputModel));
     ASSERT_NE(function, nullptr);
+//    std::cout << "Ordered ops names\n";
+//    for (const auto &n : function->get_ordered_ops()) {
+//        std::cout << "----" << n->get_friendly_name() << "---\n";
+//    }
 }
 
-TEST_P(PDPDBasicTest, testInputModel_getInputs)
+TEST_P(FrontEndBasicTest, testInputModel_getInputsOutputs)
 {
     ASSERT_NO_THROW(doLoadFromFile());
 
+    using CustomCheck = std::function<void(Place::Ptr)>;
+    auto checkPlaces = [&](const std::vector<Place::Ptr>& places, CustomCheck cb) {
+        EXPECT_GT(places.size(), 0);
+        std::set<Place::Ptr> placesSet(places.begin(), places.end());
+        EXPECT_EQ(placesSet.size(), places.size());
+        std::for_each(places.begin(), places.end(), [&](Place::Ptr place) {
+            ASSERT_NE(place, nullptr);
+            EXPECT_GT(place->getNames().size(), 0);
+            cb(place);
+        });
+    };
     std::vector<Place::Ptr> inputs;
     ASSERT_NO_THROW(inputs = m_inputModel->getInputs());
-    EXPECT_GT(inputs.size(), 0);
-    std::set<Place::Ptr> inputsSet(inputs.begin(), inputs.end());
-    EXPECT_EQ(inputsSet.size(), inputs.size());
-    std::for_each(inputs.begin(), inputs.end(), [](Place::Ptr place) {
-        ASSERT_NE(place, nullptr);
-        EXPECT_GT(place->getNames().size(), 0);
+    checkPlaces(inputs, [&](Place::Ptr place) {
         //TODO: shall we verify that each 'place->isInput() returns true'?
     });
-}
-
-TEST_P(PDPDBasicTest, testInputModel_getOutputs)
-{
-    ASSERT_NO_THROW(doLoadFromFile());
 
     std::vector<Place::Ptr> outputs;
     ASSERT_NO_THROW(outputs = m_inputModel->getOutputs());
-    EXPECT_GT(outputs.size(), 0);
-    std::set<Place::Ptr> outputsSet(outputs.begin(), outputs.end());
-    EXPECT_EQ(outputsSet.size(), outputs.size());
-    std::for_each(outputs.begin(), outputs.end(), [](Place::Ptr place) {
-        EXPECT_NE(place, nullptr);
-        EXPECT_GT(place->getNames().size(), 0);
+    checkPlaces(outputs, [&](Place::Ptr place) {
         //TODO: shall we verify that each 'place->isOutput() returns true'?
     });
 }
 
-TEST_P(PDPDBasicTest, DISABLED_testInputModel_getPlaceByTensorName)
+TEST_P(FrontEndBasicTest, testInputModel_getPlaceByTensorName)
 {
     ASSERT_NO_THROW(doLoadFromFile());
 
-    // TODO: not clear now
-}
-
-TEST_P(PDPDBasicTest, testInputModel_overrideAllOutputs)
-{
-    ASSERT_NO_THROW(doLoadFromFile());
+    auto testGetPlaceByTensorName = [&](const std::vector<Place::Ptr>& places) {
+        EXPECT_GT(places.size(), 0);
+        std::for_each(places.begin(), places.end(), [&](Place::Ptr place) {
+            //TODO: check if this expectation is correct
+            for (auto name : place->getNames()) {
+                EXPECT_EQ(place, m_inputModel->getPlaceByTensorName(name));
+            }
+        });
+    };
 
     std::vector<Place::Ptr> outputs;
     ASSERT_NO_THROW(outputs = m_inputModel->getOutputs());
-    std::set<Place::Ptr> outputsSet(outputs.begin(), outputs.end());
-
-    auto outputsReversed = outputs;
-    std::reverse(outputsReversed.begin(), outputsReversed.end());
-    ASSERT_NO_THROW(m_inputModel->overrideAllOutputs(outputsReversed));
-    ASSERT_NO_THROW(outputs = m_inputModel->getOutputs());
-    EXPECT_GT(outputs.size(), 0);
-    std::set<Place::Ptr> outputsSetAfter(outputs.begin(), outputs.end());
-    EXPECT_EQ(outputsSet.size(), outputsSetAfter.size());
-    std::for_each(outputs.begin(), outputs.end(), [&](Place::Ptr place) {
-        EXPECT_GT(outputsSet.count(place), 0);
-    });
-}
-
-TEST_P(PDPDBasicTest, testInputModel_overrideAllOutputs_empty)
-{
-    ASSERT_NO_THROW(doLoadFromFile());
-
-    std::vector<Place::Ptr> outputs;
-    ASSERT_NO_THROW(outputs = m_inputModel->getOutputs());
-    ASSERT_NO_THROW(m_inputModel->overrideAllOutputs({}));
-    ASSERT_NO_THROW(outputs = m_inputModel->getOutputs());
-    ASSERT_EQ(outputs.size(), 0);
-    // TODO: should we check that all previous outputs will return false to Place::isOutput()?
-}
-
-TEST_P(PDPDBasicTest, testInputModel_overrideAllInputs)
-{
-    ASSERT_NO_THROW(doLoadFromFile());
+    testGetPlaceByTensorName(outputs);
 
     std::vector<Place::Ptr> inputs;
     ASSERT_NO_THROW(inputs = m_inputModel->getInputs());
-    std::set<Place::Ptr> inputsSet(inputs.begin(), inputs.end());
-
-    auto inputsReversed = inputs;
-    std::reverse(inputsReversed.begin(), inputsReversed.end());
-    ASSERT_NO_THROW(m_inputModel->overrideAllInputs(inputsReversed));
-    ASSERT_NO_THROW(inputs = m_inputModel->getInputs());
-    EXPECT_GT(inputs.size(), 0);
-    std::set<Place::Ptr> inputsSetAfter(inputs.begin(), inputs.end());
-    EXPECT_EQ(inputsSet.size(), inputsSetAfter.size());
-    std::for_each(inputs.begin(), inputs.end(), [&](Place::Ptr place) {
-        EXPECT_GT(inputsSet.count(place), 0);
-    });
+    testGetPlaceByTensorName(inputs);
 }
 
-TEST_P(PDPDBasicTest, testInputModel_overrideAllInputs_empty)
+TEST_P(FrontEndBasicTest, testInputModel_overrideAll)
 {
     ASSERT_NO_THROW(doLoadFromFile());
 
-    std::vector<Place::Ptr> inputs;
-    ASSERT_NO_THROW(inputs = m_inputModel->getInputs());
-    ASSERT_NO_THROW(m_inputModel->overrideAllInputs({}));
-    ASSERT_NO_THROW(inputs = m_inputModel->getInputs());
-    ASSERT_EQ(inputs.size(), 0);
-    // TODO: should we check that all previous outputs will return false to Place::isInput()?
+    using GetPlaces = std::function<std::vector<Place::Ptr>()>;
+    using OverridePlaces = std::function<void(const std::vector<Place::Ptr>&)>;
+    auto verifyOverride = [](GetPlaces getCB, OverridePlaces overrideCB) {
+        std::vector<Place::Ptr> places;
+        ASSERT_NO_THROW(places = getCB());
+        std::set<Place::Ptr> placesSet(places.begin(), places.end());
+
+        auto placesReversed = places;
+        std::reverse(placesReversed.begin(), placesReversed.end());
+        ASSERT_NO_THROW(overrideCB(placesReversed));
+        ASSERT_NO_THROW(places = getCB());
+        EXPECT_GT(places.size(), 0);
+        std::set<Place::Ptr> placesSetAfter(places.begin(), places.end());
+        EXPECT_EQ(placesSet.size(), placesSet.size());
+        std::for_each(places.begin(), places.end(), [&](Place::Ptr place) {
+            EXPECT_GT(placesSet.count(place), 0);
+        });
+    };
+    verifyOverride([&]() { return m_inputModel->getInputs(); },
+                   [&](const std::vector<Place::Ptr>& p) { m_inputModel->overrideAllInputs(p); });
+
+    verifyOverride([&]() { return m_inputModel->getOutputs(); },
+                   [&](const std::vector<Place::Ptr>& p) { m_inputModel->overrideAllOutputs(p); });
 }
 
-TEST_P(PDPDBasicTest, DISABLED_testInputModel_extractSubgraph)
+TEST_P(FrontEndBasicTest, testInputModel_overrideAll_empty)
+{
+    ASSERT_NO_THROW(doLoadFromFile());
+    using GetPlaces = std::function<std::vector<Place::Ptr>()>;
+    using OverrideEmpty = std::function<void(void)>;
+    using CustomCheck = std::function<void(Place::Ptr)>;
+    auto verifyOverride = [](GetPlaces getCB, OverrideEmpty overrideCB, CustomCheck customCB) {
+        std::vector<Place::Ptr> places;
+        std::vector<Place::Ptr> placesAfter;
+        ASSERT_NO_THROW(places = getCB());
+        ASSERT_NO_THROW(overrideCB());
+        ASSERT_NO_THROW(placesAfter = getCB());
+        ASSERT_EQ(placesAfter.size(), 0);
+        std::for_each(places.begin(), places.end(), [&](Place::Ptr place) { customCB(place); });
+    };
+    verifyOverride([&]() { return m_inputModel->getOutputs(); },
+                   [&]() { m_inputModel->overrideAllOutputs({}); },
+                   [&](Place::Ptr place) {
+                       // TODO: should we check that all previous outputs will return false to Place::isOutput()?
+                   });
+
+    verifyOverride([&]() { return m_inputModel->getInputs(); },
+                   [&]() { m_inputModel->overrideAllInputs({}); },
+                   [&](Place::Ptr place) {
+                       // TODO: should we check that all previous outputs will return false to Place::isInput()?
+                   });
+}
+
+TEST_P(FrontEndBasicTest, DISABLED_testInputModel_extractSubgraph)
 {
     ASSERT_NO_THROW(doLoadFromFile());
 
     // TODO: not clear now
 }
 
-TEST_P(PDPDBasicTest, DISABLED_testInputModel_setPartialShape)
+TEST_P(FrontEndBasicTest, DISABLED_testInputModel_setPartialShape)
 {
     ASSERT_NO_THROW(doLoadFromFile());
 
     // TODO: not clear now
 }
+// ----- End of common part for all frontends -----
+
+// This part is frontend-specific
+using PDPDBasicTest = FrontEndBasicTest;
 
 static const std::vector<std::string> models {
         std::string("conv2d"),
         std::string("conv2d_s/conv2d.pdmodel"),
+        std::string("conv2d_relu/conv2d_relu.pdmodel"),
 };
 
-INSTANTIATE_TEST_CASE_P(PDPDBasicTest, PDPDBasicTest,
+INSTANTIATE_TEST_CASE_P(PDPDBasicTest, FrontEndBasicTest,
                         ::testing::ValuesIn(models),
                         getTestCaseName);
--- ngraph/test/frontend/paddlepaddle/model_specific.cpp
+++ ngraph/test/frontend/paddlepaddle/model_specific.cpp
@@ -44,8 +44,75 @@ public:
 TEST_F(PDPDModelsTest, testConv2D)
 {
     ASSERT_NO_THROW(doLoadFromFile("conv2d"));
-    // TODO: check all inputs/outputs for conv2d
     ASSERT_NO_THROW(m_inputModel->getInputs());
+    ASSERT_NO_THROW(m_inputModel->getOutputs());
     ASSERT_EQ(m_inputModel->getInputs().size(), 1);
     ASSERT_EQ(m_inputModel->getOutputs().size(), 1);
+
+    auto input = m_inputModel->getInputs().front();
+    auto output = m_inputModel->getOutputs().front();
+    ASSERT_TRUE(input->isInput());
+    ASSERT_FALSE(input->isOutput());
+    ASSERT_FALSE(output->isInput());
+    ASSERT_TRUE(output->isOutput());
+    ASSERT_FALSE(input->isEqual(output));
+    ASSERT_FALSE(output->isEqual(input));
+
+    // Verify input is 'x' (see generating script)
+    ASSERT_EQ(input->getNames().size(), 1);
+    auto inputName = input->getNames().front();
+    EXPECT_EQ(inputName, "x");
+
+    // Verify output name
+    ASSERT_EQ(output->getNames().size(), 1);
+    auto outputName = output->getNames().front();
+    EXPECT_EQ(outputName, "save_infer_model/scale_0.tmp_0");
+}
+
+TEST_F(PDPDModelsTest, testConv2D_relu_cut_outputs)
+{
+    ASSERT_NO_THROW(doLoadFromFile("conv2d_relu/conv2d_relu.pdmodel"));
+    std::shared_ptr<ngraph::Function> function;
+    ASSERT_NO_THROW(function = m_frontEnd->convert(m_inputModel));
+    // Ensure that it contains 'relu'
+    EXPECT_TRUE(std::find_if(function->get_ordered_ops().begin(), function->get_ordered_ops().end(),
+                             [&](const std::shared_ptr<ngraph::Node>& node) {
+                                 return node->get_friendly_name().find("relu") != std::string::npos;
+                             }) != function->get_ordered_ops().end());
+
+    Place::Ptr place;
+    ASSERT_NO_THROW(place = m_inputModel->getPlaceByTensorName("conv2d_0.tmp_0"));
+    ASSERT_NE(place, nullptr);
+    ASSERT_NO_THROW(m_inputModel->overrideAllOutputs({place}));
+
+    ASSERT_NO_THROW(function = m_frontEnd->convert(m_inputModel));
+    // Verify that after override nGraph function doesn't have 'relu' anymore
+    EXPECT_FALSE(std::find_if(function->get_ordered_ops().begin(), function->get_ordered_ops().end(),
+                              [&](const std::shared_ptr<ngraph::Node>& node) {
+                                  return node->get_friendly_name().find("relu") != std::string::npos;
+                              }) != function->get_ordered_ops().end());
+}
+
+TEST_F(PDPDModelsTest, testConv2D_conv2d_cut_inputs)
+{
+    ASSERT_NO_THROW(doLoadFromFile("conv2d_relu/conv2d_relu.pdmodel"));
+    std::shared_ptr<ngraph::Function> function;
+    ASSERT_NO_THROW(function = m_frontEnd->convert(m_inputModel));
+    // Ensure that it contains 'conv2d'
+    EXPECT_TRUE(std::find_if(function->get_ordered_ops().begin(), function->get_ordered_ops().end(),
+                             [&](const std::shared_ptr<ngraph::Node>& node) {
+                                 return node->get_friendly_name().find("conv2d") != std::string::npos;
+                             }) != function->get_ordered_ops().end());
+
+    Place::Ptr place;
+    ASSERT_NO_THROW(place = m_inputModel->getPlaceByTensorName("relu_0.tmp_0"));
+    ASSERT_NE(place, nullptr);
+    ASSERT_NO_THROW(m_inputModel->overrideAllInputs({place}));
+
+    ASSERT_NO_THROW(function = m_frontEnd->convert(m_inputModel));
+    // Verify that after override nGraph function doesn't have 'conv2d' anymore
+    EXPECT_FALSE(std::find_if(function->get_ordered_ops().begin(), function->get_ordered_ops().end(),
+                              [&](const std::shared_ptr<ngraph::Node>& node) {
+                                  return node->get_friendly_name().find("conv2d") != std::string::npos;
+                              }) != function->get_ordered_ops().end());
 }
