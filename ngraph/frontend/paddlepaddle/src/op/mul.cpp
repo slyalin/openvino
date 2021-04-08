bdeb87c97efa37eaead8fe189d21ff3778121c99
jialipen <cecilia.peng@intel.com>
2021-04-08 19:11:49 +0800

    message for assert

--- ngraph/frontend/paddlepaddle/src/op/mul.cpp
+++ ngraph/frontend/paddlepaddle/src/op/mul.cpp
@@ -26,10 +26,10 @@ namespace op {
 OutputVector mul (const NodeContext& node) {
     auto x = node.get_ng_input("X");
     auto y = node.get_ng_input("Y");
-    MY_ASSERT(x.get_partial_shape().rank().is_static());
+    MY_ASSERT(x.get_partial_shape().rank().is_static(), "matmul: X rank must be static!");
     int64_t x_rank = x.get_partial_shape().rank().get_length();
     MY_ASSERT(y.get_partial_shape().rank().is_static() &&
-              y.get_partial_shape().rank().get_length() == 2);
+              y.get_partial_shape().rank().get_length() == 2, "matmul: Y rank must be static, and 2!");
     if (x_rank > 2) {
         auto shape = std::make_shared<ngraph::opset6::ShapeOf>(x);
         int64_t x_num_col_dims = node.get_attribute<int32_t>("x_num_col_dims");
