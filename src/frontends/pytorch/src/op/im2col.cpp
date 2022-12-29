f675079245fb66623f6fa099922645969843b6fa
eaidova <ekaterina.aidova@intel.com>
2022-12-29 17:33:13 +0400

    use split

--- src/frontends/pytorch/src/op/im2col.cpp
+++ src/frontends/pytorch/src/op/im2col.cpp
@@ -9,32 +9,33 @@
 namespace ov {
 namespace frontend {
 namespace pytorch {
-    std::shared_ptr<Node> get_im2col_indices_along_dim(NodeContext& context,
-                                                    std::shared_ptr<Node> input_d,
-                                                    int64_t kernel_size_d,
-                                                    int64_t dilation_d,
-                                                    int64_t padding_d,
-                                                    int64_t stride_d) {
-        auto zero = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {0}));
-        auto minus_one = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {-1}));
-        auto kernel_size = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {kernel_size_d}));
-        auto padding_2 = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {padding_d * 2}));
-        auto stride = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {stride_d}));
-        auto blocks_d = context.mark_node(std::make_shared<opset8::Add>(input_d, padding_2));
-        auto subtrahend =
-            context.mark_node(opset8::Constant::create(element::i64, Shape{}, {dilation_d * (kernel_size_d - 1)}));
-        blocks_d = context.mark_node(std::make_shared<opset8::Subtract>(blocks_d, subtrahend));
-        auto blocks_d_indices = context.mark_node(std::make_shared<opset8::Range>(zero, blocks_d, stride, element::i64));
-        blocks_d_indices = context.mark_node(std::make_shared<opset8::Unsqueeze>(blocks_d_indices, zero));
-        std::vector<int64_t> rng;
-        for (int64_t i = 0; i < kernel_size_d * dilation_d; i += dilation_d) {
-            rng.push_back(i);
-        }
-
-        auto kernel_grid = context.mark_node(opset8::Constant::create(element::i64, Shape{rng.size()}, rng));
-        auto kernel_mask = context.mark_node(std::make_shared<opset8::Unsqueeze>(kernel_grid, minus_one));
-        return context.mark_node(std::make_shared<opset8::Add>(blocks_d_indices, kernel_mask));
+std::shared_ptr<Node> get_im2col_indices_along_dim(NodeContext& context,
+                                                   ov::Output<Node> input_d,
+                                                   int64_t kernel_size_d,
+                                                   int64_t dilation_d,
+                                                   int64_t padding_d,
+                                                   int64_t stride_d) {
+    auto zero = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {0}));
+    auto minus_one = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {-1}));
+    auto kernel_size = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {kernel_size_d}));
+    auto padding_2 = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {padding_d * 2}));
+    auto stride = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {stride_d}));
+    auto input_d_squeezed = context.mark_node(std::make_shared<opset8::Squeeze>(input_d, zero));
+    auto blocks_d = context.mark_node(std::make_shared<opset8::Add>(input_d_squeezed, padding_2));
+    auto subtrahend =
+        context.mark_node(opset8::Constant::create(element::i64, Shape{}, {dilation_d * (kernel_size_d - 1)}));
+    blocks_d = context.mark_node(std::make_shared<opset8::Subtract>(blocks_d, subtrahend));
+    auto blocks_d_indices = context.mark_node(std::make_shared<opset8::Range>(zero, blocks_d, stride, element::i64));
+    blocks_d_indices = context.mark_node(std::make_shared<opset8::Unsqueeze>(blocks_d_indices, zero));
+    std::vector<int64_t> rng;
+    for (int64_t i = 0; i < kernel_size_d * dilation_d; i += dilation_d) {
+        rng.push_back(i);
     }
+
+    auto kernel_grid = context.mark_node(opset8::Constant::create(element::i64, Shape{rng.size()}, rng));
+    auto kernel_mask = context.mark_node(std::make_shared<opset8::Unsqueeze>(kernel_grid, minus_one));
+    return context.mark_node(std::make_shared<opset8::Add>(blocks_d_indices, kernel_mask));
+}
 namespace op {
 
 OutputVector translate_im2col(NodeContext& context) {
@@ -53,14 +54,11 @@ OutputVector translate_im2col(NodeContext& context) {
     auto minus_one = context.mark_node(opset8::Constant::create(element::i64, Shape{1}, {-1}));
     auto two = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {2}));
     auto four = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {4}));
-    auto b_dim = zero;
-    auto c_dim = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {1}));
-    auto h_dim = two;
-    auto w_dim = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {3}));
-    auto input_c = context.mark_node(std::make_shared<opset8::Gather>(input_shape, c_dim, zero));
-    auto input_b = context.mark_node(std::make_shared<opset8::Gather>(input_shape, b_dim, zero));
-    auto input_h = context.mark_node(std::make_shared<opset8::Gather>(input_shape, h_dim, zero));
-    auto input_w = context.mark_node(std::make_shared<opset8::Gather>(input_shape, w_dim, zero));
+    auto input_shape_split = context.mark_node(std::make_shared<opset8::Split>(input_shape, zero, 4));
+    auto input_b = input_shape_split->output(0);
+    auto input_c = input_shape_split->output(1);
+    auto input_h = input_shape_split->output(2);
+    auto input_w = input_shape_split->output(3);
     auto stride_h = stride[0];
     auto stride_w = stride[1];
     auto padding_h = padding[0];
@@ -72,11 +70,10 @@ OutputVector translate_im2col(NodeContext& context) {
     auto blocks_row_indices = get_im2col_indices_along_dim(context, input_h, kernel_h, dilation_h, padding_h, stride_h);
     auto blocks_col_indices = get_im2col_indices_along_dim(context, input_w, kernel_w, dilation_w, padding_w, stride_w);
     auto kernel_window = context.mark_node(opset8::Constant::create(element::i64, Shape{}, {kernel_h * kernel_w}));
-    auto channel_unfolded = context.mark_node(std::make_shared<opset8::Multiply>(input_c, kernel_window));
-    auto input_b_unsqueezed = context.mark_node(std::make_shared<opset8::Unsqueeze>(input_b, zero));
+    auto input_c_squeezed = context.mark_node(std::make_shared<opset8::Squeeze>(input_c, zero));
+    auto channel_unfolded = context.mark_node(std::make_shared<opset8::Multiply>(input_c_squeezed, kernel_window));
     auto channel_unfolded_unsqueezed = context.mark_node(std::make_shared<opset8::Unsqueeze>(channel_unfolded, zero));
-    auto output_shape = context.mark_node(
-        std::make_shared<opset8::Concat>(NodeVector{input_b_unsqueezed, channel_unfolded_unsqueezed, minus_one}, 0));
+    auto output_shape = context.mark_node(std::make_shared<opset8::Concat>(OutputVector{input_b, channel_unfolded_unsqueezed, minus_one}, 0));
     auto pads = context.mark_node(
         opset8::Constant::create(element::i64, Shape{4}, std::vector<int64_t>{0, 0, padding_h, padding_w}));
     auto padded_input =
