b4f2460325814ea10138709289de85ac51a3b146
Kazantsev, Roman <roman.kazantsev@intel.com>
2023-03-02 14:32:14 +0400

    [TF FE] Convert a model with Framework nodes
    Now the conversion pipeline will convert all unsupported operations to Framework nodes
    It is done with a hope that sub-graphs with Framework Nodes will be cut in later stages
    like auto-pruning.
    
    Signed-off-by: Kazantsev, Roman <roman.kazantsev@intel.com>

--- src/frontends/tensorflow/tests/convert_tricky_models.cpp
+++ src/frontends/tensorflow/tests/convert_tricky_models.cpp
@@ -335,3 +335,15 @@ TEST_F(TransformationTestsF, ModelWithLookupTableOperations) {
         model_ref = make_shared<Model>(OutputVector{add}, ParameterVector{x});
     }
 }
+
+TEST_F(TransformationTestsF, ModelWithIteratorGetNextAndUnsupportedOp) {
+    { model = convert_model("unsupported_op_itergetnext/unsupported_op_itergetnext.pb"); }
+    {
+        // create then branch body graph
+        auto x = make_shared<Parameter>(f32, Shape{2, 3});
+        auto y = make_shared<Parameter>(f32, Shape{3});
+        auto add = make_shared<Add>(x, y);
+
+        model_ref = make_shared<Model>(OutputVector{add}, ParameterVector{x, y});
+    }
+}
