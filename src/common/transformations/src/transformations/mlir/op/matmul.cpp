a72084ca13deac4e54cd6ef258b70fc211177225
Sergey Lyalin <sergey.lyalin@intel.com>
2024-07-15 16:42:31 +0000

    Use fill to prepare output for MatMul result, fixed dynamic output dimensions (transpose_b wasn't handled).

--- src/common/transformations/src/transformations/mlir/op/matmul.cpp
+++ src/common/transformations/src/transformations/mlir/op/matmul.cpp
@@ -18,6 +18,11 @@ using namespace ov::mlir;
 
 struct ConvertMatMul {
     void operator()(ConversionContext& context, NodePtr node) {
+        auto matmul_node = std::dynamic_pointer_cast<ov::op::v0::MatMul>(node);
+        assert(matmul_node);
+        // FIXME: current code limitation
+        assert(!matmul_node->get_transpose_a() && matmul_node->get_transpose_b());
+
         auto loc = createLocation(context.context, node);
         auto& builder = context.builder();
         // TODO: Support broadcasts
@@ -25,27 +30,21 @@ struct ConvertMatMul {
         const auto ov_output_element_type = node->get_output_element_type(0);
         const auto ov_output_shape = node->get_output_partial_shape(0);
         auto outType = importTensor(context.context, ov_output_shape, ov_output_element_type); // Instead of this (WRONG): cast<mlir::ShapedType>(inputs[0].getType());
-        // Named binary ops directly overwrite data in `outs` buffer so, there is no need to provide non-empty
-        // destination at the tensor-level.
-        // Use `tensor.empty` to avoid temporary buffer allocation and memcpy after bufferization.
+
         llvm::SmallVector<Value> dynamicSizes;
         for (auto [idx, dim] : llvm::enumerate(outType.getShape())) {
             if (!mlir::ShapedType::isDynamic(dim))
                 continue;
-            auto dimSize = builder.create<tensor::DimOp>(loc, idx == 0 ? inputs[0] : inputs[1], idx);  // TODO: Use symbols instead of taking dims directly from inputs
+            // FIXME: correct in case if (!transpose_a && transpose_b)
+            auto dimSize = builder.create<tensor::DimOp>(loc, idx == 0 ? inputs[0] : inputs[1], 1);  // TODO: Use symbols instead of taking dims directly from inputs
             dynamicSizes.push_back(dimSize);
         }
         auto empty = builder.create<tensor::EmptyOp>(loc, outType, dynamicSizes);
-        auto matmul_node = std::dynamic_pointer_cast<ov::op::v0::MatMul>(node);
-        assert(matmul_node);
-
-        // FIXME: current code limitation
-        assert(!matmul_node->get_transpose_a() && matmul_node->get_transpose_b());
-
-        mlir::Operation* op = nullptr;
+        auto zero = mlir::Value(getConstant(builder, ov_output_element_type, 0));
+        auto fill = builder.create<linalg::FillOp>(loc, mlir::ValueRange{zero}, mlir::ValueRange{empty});
         // TODO: Add other variants of transpose_a/transpose_b
-        op = builder.create<linalg::MatmulTransposeBOp>(loc, mlir::ValueRange{inputs[0], inputs[1]}, mlir::ValueRange{empty});
-        context.addOutputs(node, op);
+        auto matmul = builder.create<linalg::MatmulTransposeBOp>(loc, mlir::ValueRange{inputs[0], inputs[1]}, mlir::ValueRange{fill.getResult(0)});
+        context.addOutputs(node, matmul);
     }
 };
 
