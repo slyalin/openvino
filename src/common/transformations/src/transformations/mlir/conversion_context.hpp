ca540916f22c28e0c57a5f25f92d656c6d9997ea
Sergey Lyalin <sergey.lyalin@intel.com>
2024-07-15 11:00:38 +0000

    Moved ConversionContext to a separate file

---
+++ src/common/transformations/src/transformations/mlir/conversion_context.hpp
@@ -0,0 +1,49 @@
+// Copyright (C) 2018-2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include "mlir/IR/Value.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Builders.h"
+
+#include "typedefs.hpp"
+
+namespace ov {
+namespace mlir {
+
+using ::mlir::Value;
+using ::mlir::MLIRContext;
+using ::mlir::OpBuilder;
+using ::mlir::Operation;
+using ::mlir::SmallVector;
+
+class ConversionContext {
+    static std::string rt_info_convertor ();
+
+public:
+    using Convertor = std::function<void(ConversionContext&, NodePtr)>;
+    using NodeOutputMap = std::map<ov::Output<ov::Node>, mlir::Value>;
+
+    static const std::map<ov::DiscreteTypeInfo, Convertor> convertors;
+    mlir::MLIRContext* context;
+    mlir::OpBuilder* block_builder;
+    NodeOutputMap nodeOutputMap;
+
+    ConversionContext(mlir::MLIRContext* context, mlir::OpBuilder* block_builder);
+
+    SmallVector<mlir::Value> getInputs(NodePtr node);
+    void addOutputs(NodePtr node, mlir::Operation* op);
+
+    mlir::OpBuilder& builder() {
+        return *block_builder;
+    }
+
+    static void set_convertor(NodePtr node, const Convertor& convertor);
+
+    void convert(NodePtr node);
+};
+
+} // namespace mlir
+} // namespace ov
--- src/common/transformations/src/transformations/mlir/convert.cpp
+++ src/common/transformations/src/transformations/mlir/convert.cpp
@@ -66,6 +66,7 @@
 #include "mlir_op.hpp"
 #include "convert_common.hpp"
 #include "subgraph_tracker.hpp"
+#include "conversion_context.hpp"
 
 
 namespace {
@@ -99,53 +100,6 @@ SmallVector<mlir::Type> get_types_for_values(mlir::MLIRContext* context, const o
     return types;
 }
 
-class ConversionContext {
-public:
-    using Convertor = std::function<void(ConversionContext&, NodePtr)>;
-    using NodeOutputMap = std::map<ov::Output<ov::Node>, mlir::Value>;
-
-    static const std::map<ov::DiscreteTypeInfo, Convertor> convertors;
-    mlir::MLIRContext* context;
-    mlir::OpBuilder* block_builder;
-    NodeOutputMap nodeOutputMap;
-
-    ConversionContext(mlir::MLIRContext* context, mlir::OpBuilder* block_builder)
-        : context(context),
-          block_builder(block_builder) {}
-
-    SmallVector<mlir::Value> getInputs(NodePtr node) {
-        SmallVector<mlir::Value> out;
-        out.reserve(node->get_input_size());
-        for (const auto& input : node->inputs()) {
-            out.push_back(nodeOutputMap.at(input.get_source_output()));
-        }
-        return out;
-    }
-
-    void addOutputs(NodePtr node, mlir::Operation* op) {
-        const auto results = op->getOpResults();
-
-        OPENVINO_ASSERT(
-            results.size() == node->get_output_size(),
-            "Mismatch between original Node '{0}' number of outputs '{1}' and created number of outputs '{2}'",
-            node->get_friendly_name(),
-            node->get_output_size(),
-            results.size());
-
-        for (const auto& res : results) {
-            nodeOutputMap.emplace(node->output(res.getResultNumber()), res);
-        }
-    }
-
-    mlir::OpBuilder& builder() {
-        return *block_builder;
-    }
-
-    void convert(NodePtr node) {
-        convertors.at(node->get_type_info())(*this, node);
-    }
-};
-
 template <typename TargetOp>
 struct ConvertBinary {
     void operator()(ConversionContext& context, NodePtr node) {
@@ -170,12 +124,6 @@ struct ConvertBinary {
     }
 };
 
-const std::map<ov::DiscreteTypeInfo, ConversionContext::Convertor> ConversionContext::convertors = {
-    {ov::op::v1::Add::get_type_info_static(), Convertor(ConvertBinary<linalg::AddOp>())},
-    {ov::op::v1::Subtract::get_type_info_static(), Convertor(ConvertBinary<linalg::SubOp>())},
-    {ov::op::v1::Multiply::get_type_info_static(), Convertor(ConvertBinary<linalg::MulOp>())},
-    {ov::op::v1::Divide::get_type_info_static(), Convertor(ConvertBinary<linalg::DivOp>())},
-};
 
 mlir::OwningOpRef<mlir::ModuleOp> ngraph_to_mlir(MLIRContext* context,
                                                  const ov::OutputVector& inputs,
@@ -272,11 +220,12 @@ bool get_subgraph_mark(NodePtr node) {
 class MarkPattern : public ov::pass::MatcherPass {
 public:
     OPENVINO_RTTI("MarkPattern", "0");
-    MarkPattern(NodePtr pattern) {
-        auto callback = [](ov::pass::pattern::Matcher& m) {
+    MarkPattern(NodePtr pattern, ConversionContext::Convertor convertor) {
+        auto callback = [convertor](ov::pass::pattern::Matcher& m) {
             // TODO: support multi-node patterns marking
             auto node = m.get_match_root();
             set_subgraph_mark(node);
+            ConversionContext::set_convertor(node, convertor);
             return true;
         };
 
@@ -361,10 +310,10 @@ void injectMLIR(std::shared_ptr<ov::Model> model, MLIRContext* context) {
     using namespace ov::op;
     manager.set_per_pass_validation(false);
     manager.register_pass<ov::pass::SymbolicPropagation>();
-    manager.register_pass<MarkPattern>(elementwise_f32_binary_no_broadcast<v1::Add>());
-    manager.register_pass<MarkPattern>(elementwise_f32_binary_no_broadcast<v1::Subtract>());
-    manager.register_pass<MarkPattern>(elementwise_f32_binary_no_broadcast<v1::Multiply>());
-    manager.register_pass<MarkPattern>(elementwise_f32_binary_no_broadcast<v1::Divide>());
+    manager.register_pass<MarkPattern>(elementwise_f32_binary_no_broadcast<v1::Add>(), ConvertBinary<linalg::AddOp>());
+    manager.register_pass<MarkPattern>(elementwise_f32_binary_no_broadcast<v1::Subtract>(), ConvertBinary<linalg::SubOp>());
+    manager.register_pass<MarkPattern>(elementwise_f32_binary_no_broadcast<v1::Multiply>(), ConvertBinary<linalg::MulOp>());
+    manager.register_pass<MarkPattern>(elementwise_f32_binary_no_broadcast<v1::Divide>(), ConvertBinary<linalg::DivOp>());
     manager.register_pass<Partitioner>(context);
     manager.run_passes(model);
     model->validate_nodes_and_infer_types();
