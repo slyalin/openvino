cd88a94691e524fb8d940e9403201a9f8b3ece70
Sergey Lyalin <sergey.lyalin@intel.com>
2024-07-25 17:26:55 +0000

    Put rank == 2 restriction on MatMul conversion. Fixes PyTorch linear layer tests.

--- src/common/transformations/src/transformations/mlir/op/matmul.cpp
+++ src/common/transformations/src/transformations/mlir/op/matmul.cpp
@@ -45,10 +45,14 @@ using namespace ov::op;
 
 MatMulPattern::MatMulPattern() : MarkPattern(
     wrap_type<v0::MatMul>({any_input(), any_input()}, [](const Output<Node>& output) {
-        auto matmul_node = std::dynamic_pointer_cast<v0::MatMul>(output.get_node_shared_ptr());
-        assert(matmul_node);
+        auto node = std::dynamic_pointer_cast<v0::MatMul>(output.get_node_shared_ptr());
+        assert(node);
         // FIXME: current code limitation
-        return !matmul_node->get_transpose_a() && matmul_node->get_transpose_b();
+        return
+            !has_dynamic_rank(node) &&
+            !node->get_transpose_a() && node->get_transpose_b() &&
+            node->get_input_partial_shape(0).rank().get_length() == 2 &&
+            node->get_input_partial_shape(1).rank().get_length() == 2;
     }),
     ConvertMatMul()) {
     }
